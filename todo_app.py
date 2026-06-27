f871bfa7206f83cdf6d02fdae31ec88926057995
Joanne Oduguwa <jatoduguwa1010@gmail.com>
2026-06-27 21:26:17 +0100

    Add persistent task and user storage

--- todo_app.py
+++ todo_app.py
@@ -1,11 +1,40 @@
-# Phase 1 To-Do List
-
+#To-Do List
 tasks = []
+user = ""
 
-# user input
-user = input("Enter your name ")
-print(f"Welcome {user}!")
+#function to load user
+def loadUser():
+    try:
+        with open("user.txt", "r") as file:
+            user = file.read()
+            print(f"Welcome back {user}!!")
+    except FileNotFoundError: 
+        with open("user.txt", "w") as file: # Creates an empty user.txt file 
+            user = input("Enter your name ")
+            file.write(user)
+            print(f"Welcome {user}!")
 
+
+#function to load tasks from file
+def loadTasks():
+    try:
+        with open("tasks.txt", "r") as file:
+            content = file.read()
+            tasks.extend(content.splitlines())
+    except FileNotFoundError: 
+        with open("tasks.txt", "w") as file: # Creates an empty tasks.txt file 
+            pass
+
+#function to save tasks to file
+def saveTasks():
+    print("Saving tasks...")
+    try:
+        with open("tasks.txt", "w") as file:
+            content = "\n".join(tasks)
+            file.write(content)
+    except FileNotFoundError:
+        print("File not found")
+        
 # menu display
 def showMenu():
     print('\n--------------MENU-----------------')
@@ -13,11 +42,13 @@ def showMenu():
     print("-----------------------------------")
     print("\n1. add task\n2. remove task\n3. view tasks\n4. exit")
 
+# function to add tasks
 def addTask():
     task = input("Enter task: ")
     tasks.append(task)
     print(f"{task} has been added to your to do list")
 
+# function to view exsisting tasks
 def viewTasks():
     if not tasks:
         print(f"You have no tasks today {user}")
@@ -26,6 +57,7 @@ def viewTasks():
         for index, task in enumerate(tasks, start=1):
             print(f"{index}. {task}")
 
+# function to remove tasks
 def removeTask():
     if not tasks:
         print("There's nothing on your to do list today!")
@@ -41,6 +73,11 @@ def removeTask():
     except ValueError:
         print("Enter a valid task number")
 
+# main program
+
+loadUser()
+loadTasks()
+
 while True:
     showMenu()
     choice = input("\nEnter your choice (number): ")
@@ -52,6 +89,7 @@ while True:
     elif choice == "3":
         viewTasks()
     elif choice == "4":
+        saveTasks()
         print(f"Byee {user}! 👋")
         break
     else:
